c2c97fdb26b9cec3600d5ba1c8b28b877e464744
LUIZHVA <luizhva02@gmail.com>
2024-03-19 12:27:23 -0300

    12:28-19/03/2024

--- Front-acme/js/filmes.js
+++ Front-acme/js/filmes.js
@@ -13,11 +13,49 @@ export async function getFilme(id) {
     return data.filme
 }
 
+export async function getFotoFilmes() {
+    const url = 'http://localhost:8080/v2/acmefilmes/filme/foto_capa'
+    const response = await fetch(url)
+    const data = await response.json()
+    return data.filmes
+
+}
+
 export async function postFilme (filme) {
     const url = 'http://localhost:8080/v2/acmefilmes/filme/'
     
     const options = {
         method: 'POST',
+        headers: {
+            'Content-type' : 'Application/json',
+        },
+        body: JSON.stringify(filme)
+    }
+    const response = await fetch(url,options)
+
+    return response.ok
+}
+
+export async function putFilme (filme) {
+    const url = `http://localhost:8080/v2/acmefilmes/filme/${filme.id}`
+    
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-type' : 'Application/json',
+        },
+        body: JSON.stringify(filme)
+    }
+    const response = await fetch(url,options)
+
+    return response.ok
+}
+
+export async function deletefilme (id) {
+    const url = `http://localhost:8080/v2/acmefilmes/filme/${id}`
+    
+    const options = {
+        method: 'DELETE',
         headers: {
             'Content-type' : 'Application/json',
         },
--- Front-acme/js/main.js
+++ Front-acme/js/main.js
@@ -1,41 +1,5 @@
 import { getFilmes, getFilme, postFilme } from "./filmes.js"
 
-export function criarPagina(filme) {
-    const card = document.createElement('div')
-    card.classList.add('cardFilme')
-
-    const titulo = document.createElement('h2')
-    titulo.classList.add('titulo')
-    titulo.textContent = filme.nome
-
-    const descricao = document.createElement('p')
-    descricao.classList.add('descricao')
-    descricao.textContent = filme.sinopse
-
-    const duracao = document.createElement('time')
-    duracao.classList.add('duracao')
-    duracao.textContent = filme.duracao
-
-    const dtLanca = document.createElement('data')
-    dtLanca.classList.add('dtLanca')
-    dtLanca.textContent = filme.data_lancamento
-
-    const dtRelanca = document.createElement('data')
-    dtRelanca.classList.add('dtRelanca')
-    dtRelanca.textContent = filme.data_relancamento
-
-    const banner = document.createElement('img')
-    banner.classList.add('banner')
-    banner.textContent = filme.foto_capa
-
-    const valor = document.createElement('data')
-    valor.classList.add('valor')
-    valor.textContent = filme.valor_unitario
-
-    card.append(titulo, descricao, duracao, dtLanca, dtRelanca, banner, valor)
-    return card
-}
-
 export function criarCard(filme) {
 
     const card = document.createElement('div')
@@ -77,7 +41,7 @@ export function criarCard(filme) {
 }
 
 export async function preencherContainer() {
-    const cardsHolder = document.querySelector(cardsHolder)
+    const cardsHolder = document.getElementById("cardsHolder")
     const filmes = await getFilmes()
 
     filmes.forEach(filme => {
